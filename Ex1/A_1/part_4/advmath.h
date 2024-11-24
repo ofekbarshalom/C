9a9071e6e8e463c223a243f0487ad85e6101c3df
ofekbarshalom <ofekbarshalom@gmail.com>
2024-11-24 21:41:44 +0200

    changed

--- Ex1/A_1/part_4/advmath.h
+++ Ex1/A_1/part_4/advmath.h
@@ -2,8 +2,8 @@
 #define ADVMATH_H
 
 float power(int a, int b);
-float square_root(int a);
-int factorial(int a);
+float square_root(int num);
+int factorial(int num);
 float average(int arr[], int size);
 
 #endif
