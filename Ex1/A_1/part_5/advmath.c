6ac9626d80926364f9d78d442014911c668817c4
ofekbarshalom <ofekbarshalom@gmail.com>
2024-11-24 21:43:30 +0200

    added

--- Ex1/A_1/part_5/advmath.c
+++ Ex1/A_1/part_5/advmath.c
@@ -9,19 +9,24 @@ float power(int a,int b){
     return result;
 }
 
-float square_root(int a){
-    if(a < 0){
-        return -1; // Error for negative input
+float square_root(int num){
+    float x = num;
+    float y = 1;
+    float epsilon = 0.000001;
+    while (x - y > epsilon)
+    {
+        x = (x + y) / 2;
+        y = num / x;
     }
-    return sqrt(a);
+    return x;
 }
 
-int factorial(int a){
-    if(a < 0){
+int factorial(int num){
+    if(num < 0){
         return 0; // Error for negative numbers
     }
     int result = 1;
-    for(int i = 1; i <= a; i++){
+    for(int i = 1; i <= num; i++){
         result *= i;
     }
     return result;
