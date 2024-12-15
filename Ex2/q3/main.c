c8f229b62ff0894f935299c50996f97aba8685aa
ofekbarshalom <ofekbarshalom@gmail.com>
2024-12-15 17:33:33 +0200

    q3

--- Ex2/q3/main.c
+++ Ex2/q3/main.c
@@ -1,42 +1,46 @@
 #include <stdio.h>
 #define n 2
 
-void transpose(int mat[n][n]){
+void transpose(int mat[n][n]) {
     // Perform the transpose
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            // Swap mat[i][j] with mat[j][i]
             int temp = mat[i][j];
             mat[i][j] = mat[j][i];
             mat[j][i] = temp;
         }
     }
 
-    // print the transposed matrix
-    printf("the matrix you entered is:\n");
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            printf("%d", mat[i][j]);
+    // Print the transposed matrix
+    printf("The transpose of the matrix is:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", mat[i][j]);
         }
         printf("\n");
     }
 }
 
+int main() {
+    int mat[n][n]; // an n x n matrix
 
-int main(){
-    int mat[n][n]; // a 2D array of n by n
-    // init the matrix
-    printf("Enter numbers to fill the matrix");
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            scanf("%d", &mat[i][j]);
+    // initialize the matrix
+    printf("Enter numbers to fill the matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (scanf("%d", &mat[i][j]) != 1) {
+                printf("Invalid input. Please enter integers only.\n");
+                return 1;
+            }
         }
     }
 
-    //print the matrix
-    printf("the matrix you entered is:\n");
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            printf("%d", mat[i][j]);
+    // print the matrix
+    printf("The matrix you entered is:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", mat[i][j]);
         }
         printf("\n");
     }
