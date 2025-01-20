6b95a73637ea7b4d0b582c210f617a2f23968118
ofekbarshalom <ofekbarshalom@gmail.com>
2025-01-20 12:39:31 +0200

    c

--- Ex1/circle.c
+++ Ex1/circle.c
@@ -3,12 +3,15 @@
 #define PI 3.14
 #define RADIUS 3.5
 
-int main(){
+void compute(){
     double area = PI * pow(RADIUS, 2);
     double perimeter = 2 * PI * RADIUS;
     
     printf("The area is: %.2f \n The perimeter is: %.2f", area, perimeter);
+}
 
+int main(){
+    compute();
     return 0;
 }
 
