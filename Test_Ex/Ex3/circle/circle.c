3a3093b50c6ce5e6db30c27d8136596165c53dc3
ofekbarshalom <ofekbarshalom@gmail.com>
2025-01-20 13:57:25 +0200

    c

--- Test_Ex/Ex3/circle/circle.c
+++ Test_Ex/Ex3/circle/circle.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 #define PI 3.14
-#define RADIUS 3.5
+
 
 void compute(){
-    double area = PI * pow(RADIUS, 2);
-    double perimeter = 2 * PI * RADIUS;
+    double r = 3.5;
+    double area = PI * pow(r, 2);
+    double perimeter = 2 * PI * r;
     
     printf("The area is: %.2f \n The perimeter is: %.2f", area, perimeter);
 }
---
+++ Test_Ex/Ex3/while_loops/loops.h
@@ -0,0 +1,1 @@
+int countLoops(int num);
