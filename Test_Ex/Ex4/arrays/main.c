ed05ee31bc97af9898ff4a3fcd6a4c03bce81dc8
ofekbarshalom <ofekbarshalom@gmail.com>
2025-01-20 18:56:15 +0200

    c

--- Test_Ex/Ex4/arrays/main.c
+++ Test_Ex/Ex4/arrays/main.c
@@ -2,7 +2,7 @@
 
 void getNumbers(){
     int num = 0;
-    int arr[5];
+    int arr[5] = {0};
     int index = 0;
     while(num != 99){
         printf("Enter a number: ");
@@ -10,8 +10,15 @@ void getNumbers(){
 
         arr[index %5 ] = num;
     }
+    
+    printf("The array:\n");
+
+    for(int i = 0; i < 5; i++){
+        printf("arr[%d] = %d",i,arr[i]);
+    }
 }
 
 int main(){
+    getNumbers();
     return 0;
 }
