d351e953dc8490a9f4f1f7ca08c6782b25d6b448
ofekbarshalom <ofekbarshalom@gmail.com>
2025-01-22 16:12:22 +0200

    C

--- Test_Ex/Ex10/compFiles/main.c
+++ Test_Ex/Ex10/compFiles/main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int fileComperator(FILE* file1, FILE* file2){
+
+    file1 = fopen("file1.txt","r");
+    file2 = fopen("file2.txt","r");
+
+    if(file1 == NULL || file2 == NULL){
+        printf("File could not be opened\n");
+        return -1;
+    }
+
+    while(feof(file1) == EOF){
+        char file1_char, file2_char;
+        fread(&file1_char, sizeof(char), 1, file1);
+        fread(&file2_char, sizeof(char), 1, file2);
+
+        if(file1_char != file1_char){
+            return 0;
+        }
+    }
+    return 1;
+}
+
 int main(){
-    
+    FILE *file1, *file2;
+
+    int result = fileComperator(file1, file2);
+    printf("result: %d", result);
     return 0;
 }
