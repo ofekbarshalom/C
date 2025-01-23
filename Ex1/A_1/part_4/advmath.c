0b8c4eb44e4786445984a7c73176a58ebe7a700d
ofekbarshalom <ofekbarshalom@gmail.com>
2025-01-23 12:14:27 +0200

    c

--- Ex1/A_1/part_4/advmath.c
+++ Ex1/A_1/part_4/advmath.c
@@ -1,10 +1,12 @@
 #include "advmath.h"
 
-float power(int a,int b){
+float power(int a, int b){
     float result = 1.0;
+
     for(int i = 0; i < b; i++){
         result *= a;
     }
+
     return result;
 }
 
@@ -21,24 +23,21 @@ float square_root(int num){
 }
 
 int factorial(int num){
-    if(num < 0){
-        return 0; // Error for negative numbers
-    }
     int result = 1;
-    for(int i = 1; i <= num; i++){
+
+    for(int i = 1; i < num; i++){
         result *= i;
     }
+
     return result;
 }
 
 float average(int arr[], int size){
-    if(size <= 0){
-        return 0; // Avoid division by zero
-    }
-    int sum = 0;
-    for (int i = 0; i < size; i++)
-    {
+    float sum = 0;
+
+    for(int i = 0; i < size; i++){
         sum += arr[i];
     }
-    return (float)sum / size; // Cast sum to float 
+
+    return sum / size;
 }
